81707e0b3906df5e711f2bd5a74e8bf2224d40af
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-02-19 01:52:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointment from "./pages/MyAppointment";
+import Appointments from "./pages/Appointments";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="mx-4 sm:mx-[10%]">
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointment" element={<MyAppointment />} />
+        <Route path="/appointment/:docId" element={<Appointments />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+import { NavLink, useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+   const [showMenu , setShowMenu] = useState(false)
+   const [token , setToken] = useState(true)
+   const navigate = useNavigate();
+  return (
+    <div className='flex item-center justify-between text-sm py-3 mb-5 border-b border-b-gray-400'>
+   <img src={assets.logo_doc} alt='logo' className='w-41 h-13 cursor-pointer'/>
+   <ul className='hidden md:flex item-start gap-10 font-medium'>
+      <NavLink to={"/"}>
+         <li className='py-3'>HOME</li>
+         <hr className='border-none outline-none h-0.5 bg-orange-500 w-3/5 m-auto hidden'/>
+      </NavLink>
+      <NavLink to={"/doctors"}>
+         <li className='py-3'>DOCTORS</li>
+         <hr className='border-none outline-none h-0.5 bg-orange-500 w-3/5 m-auto hidden'/>
+      </NavLink>
+      <NavLink to={"/about"}>
+         <li className='py-3'>ABOUT</li>
+         <hr className='border-none outline-none h-0.5 bg-orange-500 w-3/5 m-auto hidden'/>
+      </NavLink>
+      <NavLink to={"/contact"}>
+         <li className='py-3'>CONTACT</li>
+         <hr className='border-none outline-none h-0.5 bg-orange-500 w-3/5 m-auto hidden'/>
+      </NavLink>
+   </ul>
+   <div className='flex item-center gap-4'>
+   {
+      token 
+      ? <div className='flex item-center gap-2 cursor-pointer group relative'>
+         <img className='w-9 h-9 my-1 rounded-full' src={assets.profile_pic} alt='profile'/>
+         <img className='w-2.5' src={assets.dropdown_icon} alt='dropdown'/>
+         <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+            <p onClick={()=>navigate("/my-profile")} className='hover:text-black cursor-pointer'>My Profile</p>
+            <p onClick={()=>navigate("/my-appointment")} className='hover:text-black cursor-pointer'>My Appointments</p>
+            <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+         </div>
+      </div>
+      </div>
+      
+      : <button onClick={()=>navigate("/login")} className='cursor-pointer bg-orange-500 py-2 text-white px-7 rounded-full font-light hidden md:block'>Create Account</button>
+      
+   }
+     
+   </div>
+    </div>
+  )
+}
+
+export default Navbar
